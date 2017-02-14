ee32b6d92cd04c2723ac81eefd6a5a27d7447106
Armaan Dhir <dhir.armaan@gmail.com>
2017-02-13 19:52:58 -0500

    initial commit

---
+++ src/main/java/com/armaandhir/moneewise/MoneewiseApplication.java
@@ -0,0 +1,12 @@
+package com.armaandhir.moneewise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneewiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneewiseApplication.class, args);
+	}
+}
